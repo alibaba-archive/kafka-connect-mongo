4491a006eb061595996663d69ec205fcf9a71320
Xu Jingxin <sailxjx@gmail.com>
2016-12-28 18:34:32 +0800

    Fix push script

--- docker/push.sh
+++ docker/push.sh
@@ -2,4 +2,7 @@
 
 . settings.sh
 
-docker push "${IMAGE_NAME}:${PACKAGE_VERSION}" "${IMAGE_NAME}:latest" "quay.io/${IMAGE_NAME}:${PACKAGE_VERSION}" "quay.io/${IMAGE_NAME}:latest"
+docker push "${IMAGE_NAME}:${PACKAGE_VERSION}"
+docker push "${IMAGE_NAME}:latest"
+docker push "quay.io/${IMAGE_NAME}:${PACKAGE_VERSION}"
+docker push "quay.io/${IMAGE_NAME}:latest"
