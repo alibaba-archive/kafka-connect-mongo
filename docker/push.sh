fa6d8ac4c9755fdd33d01cc7751f4526d055d185
Xu Jingxin <sailxjx@gmail.com>
2017-04-25 14:48:42 +0800

    Fix push script, remove timezone

--- docker/push.sh
+++ docker/push.sh
@@ -5,7 +5,7 @@
 docker push "docker-registry.teambition.net/library/${IMAGE_NAME}:${PACKAGE_VERSION}"
 docker push "docker-registry.teambition.net/library/${IMAGE_NAME}:latest"
 
-[[ $BUILD_ENV == "beta" ]] && exit 0
+[[ $PACKAGE_VERSION =~ ^[0-9\.]+$ ]] || exit 0
 
 docker push "sailxjx/${IMAGE_NAME}:${PACKAGE_VERSION}"
 docker push "sailxjx/${IMAGE_NAME}:latest"
