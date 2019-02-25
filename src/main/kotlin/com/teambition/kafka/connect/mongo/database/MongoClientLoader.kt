8799b6fd7e1ae94cda3b81e24087e91f05b315aa
Xu Jingxin <sailxjx@gmail.com>
2019-02-25 16:07:13 +0800

    Dont use ssl when not enabled in uri

--- src/main/kotlin/com/teambition/kafka/connect/mongo/database/MongoClientLoader.kt
+++ src/main/kotlin/com/teambition/kafka/connect/mongo/database/MongoClientLoader.kt
@@ -9,12 +9,16 @@ import java.util.concurrent.ConcurrentHashMap
  * @author Xu Jingxin
  */
 object MongoClientLoader {
-    private val defaultOptions = MongoClientOptions.builder()
-        .connectTimeout(1_000_000)
+    private val defaultOptions
+        get() = MongoClientOptions.builder()
+            .connectTimeout(1_000_000)
     private val clients = ConcurrentHashMap<String, MongoClient>()
 
     fun getClient(uri: String, options: MongoClientOptions.Builder = defaultOptions, reconnect: Boolean = false) =
         synchronized(clients) {
+            if ("ssl=true" !in uri) {
+                options.sslEnabled(false)
+            }
             if (reconnect) {
                 clients[uri] = MongoClient(MongoClientURI(uri, options))
                 clients[uri]
